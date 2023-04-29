0bcdf5ac0d0817f34da5adb94f2a6e3806a78abe
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2023-04-29 12:32:33 -0600

    feat: `insertEcoCheck.js`

--- src/dbms/insertEcoCheck.js
+++ src/dbms/insertEcoCheck.js
@@ -21,61 +21,45 @@ const config = {
   database,
 };
 
-// mysql
-//   .createPool(config)
-//   .then((pool) => {
-//     const queryString = `SELECT * FROM EcoCheck;`;
-//     pool.query(queryString, (error, results, fields) => {
-//     //   const product = products[i];
-
-//       if (error) throw error;
-//       //   console.log(results);
-//       console.log(fields);
-//       //   console.log(fields);
-//     });
-//     for (let i = 0; i < products.length; i++) {}
-//   })
-//   .catch((err) => {
-//     console.log("poop");
-//     console.log(err);
-//   });
-
 mysql
   .createPool(config)
   .then((pool) => {
-    for (let i = 0; i < products.length; i++) {
-      const product = products[i];
-      console.log(product);
+    const queryString = `SELECT COUNT (*) FROM EcoCheck1;`;
+    // const queryString = `CREATE TABLE EcoCheck1 (ProductID INT, WarehouseID INT, ProductSubstitutes VARCHAR(50), ProductShipmentDates VARCHAR(50), EnergyRequiredManf VARCHAR(50), AmountPackagingWaste VARCHAR(50), OverallEcoCheckScore VARCHAR(50));`;
+    pool.query(queryString, (error, results, fields) => {
+    //   const product = products[i];
 
-      const queryString = `
-        INSERT INTO EcoCheck(
-            'ProductID',
-            'WarehouseID',
-            'ProductSubstitutes',
-            'ProductShipmentDistance',
-            'EnergyRequiredManf', 
-            'AmountPackagingWaste', 
-            'OverallEcoCheckScore') 
-        VALUES(
-            ${product["ProductID"]}, 
-            ${product["WarehouseID"]}, 
-            '${product["ProductSubtitutes"]}',
-            '${product["ProductShipmentDistance"]}',
-            '${product["EnergyRequiredManf"]}',
-            '${product["AmountPackagingWaste"]}',
-            '${product["OverallEcoCheckScore"]}
-        );`;
-      pool.query(queryString, (error, results, fields) => {
-        if (error) throw error;
+      if (error) throw error;
         console.log(results);
-        //   console.log(fields);
-      });
-    }
+      console.log(fields);
+      //   console.log(fields);
+    });
+    for (let i = 0; i < products.length; i++) {}
   })
   .catch((err) => {
     console.log("poop");
     console.log(err);
   });
+
+// mysql
+//   .createPool(config)
+//   .then((pool) => {
+//     for (let i = 0; i < products.length; i++) {
+//       const product = products[i];
+//     //   console.log(product);
+
+//       const queryString = `INSERT INTO EcoCheck1(ProductID, WarehouseID, ProductSubstitutes, ProductShipmentDates, EnergyRequiredManf, AmountPackagingWaste, OverallEcoCheckScore) VALUES( ${parseInt(product["ProductID"])},  ${parseInt(product["WarehouseID"])},  '${product["ProductSubtitutes"]}', '${product["ProductShipmentDistance"]}', '${product["EnergyRequiredManf"]}', '${product["AmountPackagingWaste"]}', '${product["OverallEcoCheckScore"]}');`;
+//       pool.query(queryString, (error, results, fields) => {
+//         if (error) throw error;
+//         console.log(results);
+//         //   console.log(fields);
+//       });
+//     }
+//   })
+//   .catch((err) => {
+//     console.log("poop");
+//     console.log(err);
+//   });
 
 function truncateString(str, num) {
   if (str.length <= num) {
